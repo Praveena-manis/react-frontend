83a8b4016ec36b8a07e92fecb440efbef2c584d1
Praveena-manis <apraveenamanikandan92@gmail.com>
2024-01-27 11:19:12 +0530

    remove redux

--- src/redux/action.js
+++
@@ -1,6 +0,0 @@
-export const SET_AUTH = 'SET_AUTH';
-
-export const setAuth = (user, token) => ({
-  type: SET_AUTH,
-  payload: { user, token },
-});
--- src/redux/combineReducer.js
+++
@@ -1,5 +0,0 @@
-import { combineReducers } from "redux";
-import { userReducer } from "./userReducer";
-
-const combineReducer = combineReducers({ user: userReducer });
-export default combineReducer;
